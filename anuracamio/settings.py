2bb37be3d8bdd83b0207a07e82537f591b61c38c
LucasMichalko <lucasmenano@gmail.com>
2024-08-22 15:44:58 -0300

    Configuracion del server web

--- anuracamio/settings.py
+++ anuracamio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#tu-)zu7r7r_%co*sf5)x!qp1z$qw-=+-0!(j(k!d=lm&%9%!u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'anuracamio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/"templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
