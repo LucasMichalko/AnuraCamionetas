518080ac4013f3cbf4d660cbacc20151ad983914
LucasMichalko <lucasmenano@gmail.com>
2024-09-24 17:57:52 -0300

    bd avances

--- camionetas/models.py
+++ camionetas/models.py
@@ -1,3 +1,4 @@
 from django.db import models
 
-# Create your models here.
+class camioneta_1(models.Model):
+    rulo = models.FloatField()
--- camionetas/views.py
+++ camionetas/views.py
@@ -1,6 +1,3 @@
 from django.shortcuts import render
-from django.db import models
 
 # Create your views here.
-class camioneta_1 (models.Model):
-    rulo = models.FloatField()
