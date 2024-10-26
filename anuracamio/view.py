a261f121ca2140e634f8898019692825f87c2c38
LucasMichalko <lucasmenano@gmail.com>
2024-10-26 00:39:09 -0300

    Creacion de camionetas por formulario

--- anuracamio/view.py
+++ anuracamio/view.py
@@ -10,8 +10,21 @@ def index (request):
     return render(request, "index.html", context=context)
 
 def Create_camionetas (request):
-    new_camioneta = Camionetas.objects.create(name="071")
-    return HttpResponse ("Creado")
+    if request.method == 'GET':
+        Listar_camionetas = Camionetas.objects.all
+        context = {
+        "Listar_camionetas":Listar_camionetas,
+        }
+        return render(request, "Alta_Camionetas.html", context=context)
+
+    elif request.method == 'POST':
+      Camionetas.objects.create(name = request.POST['name'])
+      Listar_camionetas = Camionetas.objects.all
+      context = {
+        "Listar_camionetas":Listar_camionetas,
+    }
+    return render (request, "abm_camionetas.html", context=context)
+        
 
 def ABM_Camionetas(request):
     Listar_camionetas = Camionetas.objects.all
--- camionetas/admin.py
+++ camionetas/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from camionetas.models import Objetos_Camionetas, Camionetas
+
+admin.site.register(Objetos_Camionetas)
+admin.site.register(Camionetas)
+
 # Register your models here.
