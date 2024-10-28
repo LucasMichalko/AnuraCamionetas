5f2ccad88a83f0bfd5655170f928765b97328907
LucasMichalko <lucasmenano@gmail.com>
2024-10-28 14:13:39 -0300

    BD cambios 2

--- anuracamio/view.py
+++ anuracamio/view.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from camionetas.models import Camionetas
+from camionetas.models import Camionetas, Objetos_Camionetas
 
 def index (request):
     Listar_camionetas = Camionetas.objects.all
@@ -29,6 +29,23 @@ def Create_camionetas (request):
 def ABM_Camionetas(request):
     Listar_camionetas = Camionetas.objects.all
     context = {
+        "Listar_camionetas":Listar_camionetas,
+    }
+    return render (request, "abm_camionetas.html", context=context)
+
+def Create_Objects (request):
+    if request.method == 'GET':
+        Listar_camionetas = Camionetas.objects.all
+        context = {
+        "Listar_camionetas":Listar_camionetas,
+        }
+        return render(request, "Alta_Productos.html", context=context)
+
+    elif request.method == 'POST':
+      
+      Objetos_Camionetas.objects.create(name = request.POST['name'],)
+      Listar_camionetas = Camionetas.objects.all
+      context = {
         "Listar_camionetas":Listar_camionetas,
     }
     return render (request, "abm_camionetas.html", context=context)
--- camionetas/models.py
+++ camionetas/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
 class Objetos_Camionetas (models.Model):
+    id_Objetos = models.AutoField(primary_key=True)
     name = models.CharField(max_length=30)
-    camioneta_ID = models.IntegerField()
+    camioneta_ID_Producto = models.IntegerField()
     cantidad = models.FloatField(default=0)
 
 class Camionetas (models.Model):
+    id_Camionetas = models.AutoField(primary_key=True)
     name = models.CharField(max_length = 15, unique=True)
     
