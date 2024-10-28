3c87b2273e2f717bd8349c07bbd766d2b97d7d8f
LucasMichalko <lucasmenano@gmail.com>
2024-10-28 11:59:03 -0300

    avances

--- camionetas/models.py
+++ camionetas/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Objetos_Camionetas (models.Model):
     name = models.CharField(max_length=30)
     camioneta_ID = models.IntegerField()
-    cantidad = models.FloatField()
+    cantidad = models.FloatField(default=0)
 
 class Camionetas (models.Model):
     name = models.CharField(max_length = 15, unique=True)
