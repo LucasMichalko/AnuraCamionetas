80207aaafb5078f7e06260b6644b461c7fa37697
LucasMichalko <lucasmenano@gmail.com>
2024-11-02 23:08:39 -0300

    Cambios random

--- camionetas/models.py
+++ camionetas/models.py
@@ -4,7 +4,7 @@ class Objetos_Camionetas (models.Model):
     id_Objetos = models.AutoField(primary_key=True)
     name = models.CharField(max_length=30)
     camioneta_ID_Producto = models.IntegerField()
-    cantidad = models.FloatField(default=0)
+    cantidad = models.IntegerField(default=0)
 
 class Camionetas (models.Model):
     id_Camionetas = models.AutoField(primary_key=True)
